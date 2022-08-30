a0b091d79341516046446295013ba420e030a182
Nikolaev Viktor <maclauddd@gmail.com>
2022-08-31 00:38:53 +0300

    feat(main): added a simple web server

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import http from 'http';
 import { Scenes, Telegraf } from 'telegraf';
 import { getConfig } from './config/index.js';
 import { DataBaseService } from './database.js';
@@ -13,13 +14,28 @@ enum Buttons {
   ADD_PLACE = '📍 Добавить место',
 }
 
-const main = async (config: AppConfig): Promise<void> => {
+const main = (
+  config: AppConfig,
+): {
+  bot: Telegraf<Scenes.SceneContext<Scenes.SceneSessionData>>;
+  firebaseService: DataBaseService;
+} => {
   const bot = new Telegraf<Scenes.SceneContext>(config.botToken);
   const firebaseService = new DataBaseService(config);
 
   bot.launch();
   logger.info('BOT IS STARTED 🚀');
 
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+  return { bot, firebaseService };
+};
+
+const route = (
+  bot: Telegraf<Scenes.SceneContext<Scenes.SceneSessionData>>,
+  firebaseService: DataBaseService,
+): void => {
   bot.on('text', async (ctx) => {
     logger.info('message is recieved', ctx.update.message.text);
 
@@ -47,13 +63,24 @@ const main = async (config: AppConfig): Promise<void> => {
   });
 
   buttonList(bot, logger, firebaseService, Buttons.ADD_PLACE);
-
-  process.once('SIGINT', () => bot.stop('SIGINT'));
-  process.once('SIGTERM', () => bot.stop('SIGTERM'));
 };
 
 getConfig()
   .then((config) => {
-    main(config);
+    const { bot, firebaseService } = main(config);
+
+    const requestListener = function (req, res): void {
+      route(bot, firebaseService);
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(200);
+      res.end(`{"message": "${req.url}"}`);
+    };
+
+    const server = http.createServer(requestListener);
+    const port = 3000;
+    const host = 'localhost';
+    server.listen(port, host, () => {
+      console.log(`Server is running on http://${host}:${port}`);
+    });
   })
   .catch((error) => logger.error('Application not running', error));
